94c184f13fdba9f48fbedf4ba0a007ed953b16b0
Ben Travis <ben.travis@viget.com>
2015-09-12 13:27:19 -0400

    populated files based on codepen work

---
+++ app.js
@@ -0,0 +1,47 @@
+var data = {
+  "kind": "analytics#realtimeData",
+  "rows": [
+    [
+      "Canada",
+      "1"
+    ],
+    [
+      "France",
+      "4"
+    ],
+    [
+      "Italy",
+      "1"
+    ],
+    [
+      "Poland",
+      "1"
+    ],
+    [
+      "Ukraine",
+      "1"
+    ],
+    [
+      "United Kingdom",
+      "2"
+    ],
+    [
+      "United States",
+      "15"
+    ]
+  ]
+}
+
+
+var frag = document.createDocumentFragment()
+
+data.rows.forEach(function(item) {
+    var countryName = item[0]
+    var circle = document.createElement('div')
+    circle.id = "circle"
+    circle.innerText = countryName
+    frag.appendChild(circle);
+});
+
+var box = document.getElementById('box')
+box.appendChild( frag );
